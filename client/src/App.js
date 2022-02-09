7d6a0430484707f771cae1141fe3eaf9f7e72c9a
Tanvi Tendulkar <tanvi.tendulkar1@gmail.com>
2022-02-09 12:38:00 +0530

    added navbar and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import {HomePage,Error,TutorDetail,TutorList,AddTutor, Register} from './pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import {Navbar,Footer} from './components'
 function App() {
   return (
+    <div>
+    <Navbar/>
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<HomePage/>}/>
@@ -11,10 +13,10 @@ function App() {
       <Route path='/tutor-detail' element={<TutorDetail/>}/>
       <Route path='/register' element={<Register/>}/>
       <Route path='*' element={<Error/>}/>
-
-
     </Routes>
     </BrowserRouter>
+    <Footer/>
+    </div>
   );
 }
 
