d4bb54429c24b01614dd430275d83fb63d2cb9aa
Tanvi Tendulkar <tanvi.tendulkar1@gmail.com>
2023-09-12 11:17:31 +0530

    deploy4

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ const auth  = require("./server/middleware/auth.js");
 
 const app = express();
 app.use(express.json())
-app.use(
-    express.static(path.join(__dirname, "../client/build"))
-);
+// app.use(
+//     express.static(path.join(__dirname, "./client/build"))
+// );
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('./client/build'));
@@ -42,11 +42,11 @@ app.use(bodyParser.json());
 //routes
 app.use('/api/tutor',tutorRoutes.routes);
 app.use('/api/user',userRoutes.routes);
-app.get("*", (req, res) => {
-    res.sendFile(
-        path.join(__dirname, "./client/build/index.html")
-    );
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(
+//         path.join(__dirname, "./client/build/index.html")
+//     );
+// });
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
